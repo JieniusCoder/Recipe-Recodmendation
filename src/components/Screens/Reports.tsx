e975edd49f42ef4e215aa10348dcf5fed8c914dc
Jiecheng Yao <jiy921@lehigh.edu>
2024-09-16 17:20:27 -0400

    App2.0 in progress

--- src/components/Screens/Reports.tsx
+++ src/components/Screens/Reports.tsx
@@ -18,7 +18,7 @@ const Recipe: React.FC<RecipeProps> = ({ label, image, url, calories }) => {
       <h3>{label}</h3>
       <img src={image} alt={label} style={{ width: "100px" }} />
       <br />
-      <a href= {url} target="_blank" rel="noopener noreferrer">
+      <a href={url} target="_blank" rel="noopener noreferrer">
         LINK
       </a>
       <p>Calories: {calories}</p>
@@ -28,17 +28,40 @@ const Recipe: React.FC<RecipeProps> = ({ label, image, url, calories }) => {
 
 //Edaman ID: c24b1df0
 //Aplication keys: e47b0bcd58dcee39537ee987dfeea200
-
 const Reports: React.FC = () => {
   const [items, setItems] = useState<ReceiptItem[]>([]);
   const [name, setName] = useState<string>("");
   const [recipes, setRecipes] = useState<RecipeProps[]>([]);
+
+  //user recipe selection
+  const [isMaxCaloriesChecked, setIsMaxCaloriesChecked] =
+    useState<boolean>(false);
+  const [isDietChecked, setIsDietChecked] = useState<boolean>(false);
+
+  const [diet, setDiet] = useState<string[]>([]);
+  const [maxCalories, setMaxCalories] = useState<number | "">("");
+
+  const dietRestrictions = [
+    "balanced",
+    "high-protein",
+    "low-fat",
+    "low-carb",
+    "vegan",
+    "vegetarian",
+    "sugar-conscious",
+    "peanut-free",
+    "tree-nut-free",
+    "alcohol-free",
+    "gluten-free",
+  ];
 
   //Edaman Crendentials
   const APP_ID = "c24b1df0";
   const APP_KEY = "e47b0bcd58dcee39537ee987dfeea200";
 
   const getRecipes = async () => {
+    //adjust API calls for diet, health, and max calories
+
     // construct the API URL with the items names
     const API_URL = `https://api.edamam.com/api/recipes/v2?type=any&q=
     ${items
@@ -89,10 +112,31 @@ const Reports: React.FC = () => {
     setItems(newItems);
   };
 
+  //diet
+  const handleDietCheckBox = () => {
+    setIsDietChecked(!isDietChecked);
+  };
+
+  const handleDietChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedOptions = Array.from(e.target.selectedOptions, option => option.value);
+    // add new to the old selected diet restrictions
+    setDiet(selectedOptions);
+  };
+
+  //calories
+  const handleCaloriesCheckBox = () => {
+    setIsMaxCaloriesChecked(!isMaxCaloriesChecked);
+  };
+
+  const handleCaloriesChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setMaxCalories(parseInt(e.target.value));
+  };
+
   return (
     <div>
       <h1>Create Recipe</h1>
 
+      {/* add item form */}
       <form onSubmit={addItem}>
         <div>
           <label>Item Name:</label>
@@ -105,6 +149,88 @@ const Reports: React.FC = () => {
         </div>
         <button type="submit">Add Item</button>
       </form>
+
+      {/* user recipe selection */}
+      {/* Max Calories */}
+      <div>
+        <label>
+          <input
+            type="checkbox"
+            checked={isMaxCaloriesChecked}
+            onChange={handleCaloriesCheckBox}
+          />
+          Max Calories
+        </label>
+
+        {isMaxCaloriesChecked && (
+          <div>
+            <label>
+              Enter Max Calories:
+              <input
+                type="number"
+                value={maxCalories}
+                onChange={handleCaloriesChange}
+                placeholder="Enter max calories"
+              />
+            </label>
+          </div>
+        )}
+      </div>
+
+      {/* diet restriction */}
+      <div>
+        <label>
+          <input
+            type="checkbox"
+            checked={isDietChecked}
+            onChange={handleDietCheckBox}
+          />
+          Diet Restrictions
+        </label>
+
+        {isDietChecked && (
+          <div>
+            <select
+              multiple value={diet}
+              onChange={handleDietChange}
+              style={{
+                width: "50%",
+                padding: "10px",
+                fontSize: "16px",
+                border: "2px solid #ccc",
+                borderRadius: "8px",
+                backgroundColor: "#fff",
+                appearance: "none",
+                backgroundRepeat: "no-repeat",
+                backgroundPosition: "right 10px top 50%",
+                backgroundSize: "12px",
+                cursor: "pointer",
+                marginBottom: "10px",
+              }}
+            >
+              {dietRestrictions.map((restriction) => (
+                <option key={restriction} value={restriction}>
+                  {restriction}
+                </option>
+              ))}
+            </select>
+          </div>
+        )}
+      </div>
+
+      {/* display selected diet restrictions */}
+      <div>
+        {diet.length > 0 && (
+          <div>
+            <h3>Selected Diet Restrictions:</h3>
+            <p>
+              {diet.map((restriction) => (
+                <li key={restriction}>{restriction}</li>
+              ))}
+            </p>
+          </div>
+        )}
+      </div>
 
       {/* display all items */}
       <h3>Items:</h3>
@@ -136,19 +262,19 @@ const Reports: React.FC = () => {
 
       <h3>Receipt Suggestion:</h3>
       <div className="recipes">
-      {items.length > 0 ? (
-        recipes.map((recipe, index) => (
-          <Recipe
-            key={index}
-            label={recipe.label}
-            image={recipe.image}
-            url={recipe.url}
-            calories={parseInt(recipe.calories.toFixed(0))}
-          />
-        ))
-      ) : (
-        <p>No items added yet.</p>
-      )}
+        {items.length > 0 ? (
+          recipes.map((recipe, index) => (
+            <Recipe
+              key={index}
+              label={recipe.label}
+              image={recipe.image}
+              url={recipe.url}
+              calories={parseInt(recipe.calories.toFixed(0))}
+            />
+          ))
+        ) : (
+          <p>No items added yet.</p>
+        )}
       </div>
       <hr />
     </div>
