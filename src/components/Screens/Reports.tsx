b5da12fd3f338f189c54e67e48e4cdb11a1729d2
Jiecheng Yao <jiy921@lehigh.edu>
2024-09-18 11:44:03 -0400

    App 2.0 added diet restriciton

--- src/components/Screens/Reports.tsx
+++ src/components/Screens/Reports.tsx
@@ -34,25 +34,15 @@ const Reports: React.FC = () => {
   const [recipes, setRecipes] = useState<RecipeProps[]>([]);
 
   //user recipe selection
-  const [isMaxCaloriesChecked, setIsMaxCaloriesChecked] =
-    useState<boolean>(false);
   const [isDietChecked, setIsDietChecked] = useState<boolean>(false);
 
   const [diet, setDiet] = useState<string[]>([]);
-  const [maxCalories, setMaxCalories] = useState<number | "">("");
 
   const dietRestrictions = [
     "balanced",
     "high-protein",
     "low-fat",
     "low-carb",
-    "vegan",
-    "vegetarian",
-    "sugar-conscious",
-    "peanut-free",
-    "tree-nut-free",
-    "alcohol-free",
-    "gluten-free",
   ];
 
   //Edaman Crendentials
@@ -60,20 +50,26 @@ const Reports: React.FC = () => {
   const APP_KEY = "e47b0bcd58dcee39537ee987dfeea200";
 
   const getRecipes = async () => {
-    //adjust API calls for diet, health, and max calories
+    //adjust API calls for dietand max calories
+    let API_URL = "";
 
-    // construct the API URL with the items names
-    const API_URL = `https://api.edamam.com/api/recipes/v2?type=any&q=
-    ${items
-      .map((item) => item.name)
-      .join("%20")}&app_id=${APP_ID}&app_key=${APP_KEY}`;
+    // API URL
+    if(isDietChecked){
+      API_URL = `https://api.edamam.com/api/recipes/v2?type=any&q=
+      ${items
+        .map((item) => item.name)
+        .join("%20")}&app_id=${APP_ID}&app_key=${APP_KEY}&diet=${diet.join("&diet=")}`;
+    } else {
+      // no calories or diet restrictions
+      API_URL = `https://api.edamam.com/api/recipes/v2?type=any&q=
+      ${items
+        .map((item) => item.name)
+        .join("%20")}&app_id=${APP_ID}&app_key=${APP_KEY}`;
+    }
 
     const response = await fetch(API_URL);
 
     const data = await response.json();
-
-    console.log("data hits");
-    console.log(data.hits);
 
     const recipeHits = data.hits.map((hit: any) => {
       return {
@@ -84,13 +80,7 @@ const Reports: React.FC = () => {
       };
     });
 
-    console.log("recipeHits");
-    console.log(recipeHits);
-
     setRecipes(recipeHits);
-
-    console.log("recipes2");
-    console.log(recipes.map((recipe) => recipe.label));
   };
 
   const addItem = (e: React.FormEvent) => {
@@ -118,18 +108,11 @@ const Reports: React.FC = () => {
   };
 
   const handleDietChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    const selectedOptions = Array.from(e.target.selectedOptions, option => option.value);
-    // add new to the old selected diet restrictions
+    const selectedOptions = Array.from(
+      e.target.selectedOptions,
+      (option) => option.value
+    );
     setDiet(selectedOptions);
-  };
-
-  //calories
-  const handleCaloriesCheckBox = () => {
-    setIsMaxCaloriesChecked(!isMaxCaloriesChecked);
-  };
-
-  const handleCaloriesChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setMaxCalories(parseInt(e.target.value));
   };
 
   return (
@@ -151,32 +134,6 @@ const Reports: React.FC = () => {
       </form>
 
       {/* user recipe selection */}
-      {/* Max Calories */}
-      <div>
-        <label>
-          <input
-            type="checkbox"
-            checked={isMaxCaloriesChecked}
-            onChange={handleCaloriesCheckBox}
-          />
-          Max Calories
-        </label>
-
-        {isMaxCaloriesChecked && (
-          <div>
-            <label>
-              Enter Max Calories:
-              <input
-                type="number"
-                value={maxCalories}
-                onChange={handleCaloriesChange}
-                placeholder="Enter max calories"
-              />
-            </label>
-          </div>
-        )}
-      </div>
-
       {/* diet restriction */}
       <div>
         <label>
@@ -190,8 +147,10 @@ const Reports: React.FC = () => {
 
         {isDietChecked && (
           <div>
+            <h4>drag to select multiple</h4>
             <select
-              multiple value={diet}
+              multiple
+              value={diet}
               onChange={handleDietChange}
               style={{
                 width: "50%",
@@ -209,25 +168,24 @@ const Reports: React.FC = () => {
               }}
             >
               {dietRestrictions.map((restriction) => (
-                <option key={restriction} value={restriction}>
+                <option>
                   {restriction}
                 </option>
               ))}
             </select>
-          </div>
-        )}
-      </div>
-
-      {/* display selected diet restrictions */}
-      <div>
-        {diet.length > 0 && (
-          <div>
-            <h3>Selected Diet Restrictions:</h3>
-            <p>
-              {diet.map((restriction) => (
-                <li key={restriction}>{restriction}</li>
-              ))}
-            </p>
+            {/* display selected diet restrictions */}
+            <div>
+              {diet.length > 0 && (
+                <div>
+                  <h3>Selected Diet Restrictions:</h3>
+                  <p>
+                    {diet.map((restriction) => (
+                      <li key={restriction}>{restriction}</li>
+                    ))}
+                  </p>
+                </div>
+              )}
+            </div>
           </div>
         )}
       </div>
